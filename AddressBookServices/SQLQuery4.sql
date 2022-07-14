2761c2f2c7059bbf4ee5829d77a220c126079c23
shreegowri <gaganandh@gmail.com>
2022-07-14 23:48:16 +0530

    [shreegowri]ability to edit the data in the table

---
+++ AddressBookServices/SQLQuery4.sql
@@ -0,0 +1,4 @@
+﻿UPDATE AddressBook set PhoneNumber=1234567890 where FirstName='Shree';
+UPDATE AddressBook set State='Maharastra',City='Mumbai' where FirstName='Mahesh';
+
+select * from AddressBook;
