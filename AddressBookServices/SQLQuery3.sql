b6b29d9000f0882bc9aa88cd6d284fc2f07d1e32
shreegowri <gaganandh@gmail.com>
2022-07-14 23:39:09 +0530

    [shreegowri]inserting the data into the table

---
+++ AddressBookServices/SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿
+INSERT INTO AddressBook
+VALUES('Shree','Gowri','7th cross','Bangaluru','Karnataka',560065,5243454345,'abc@gmail.com'),
+('Mahesh','Koder','8th cross','Mysore','Karnataka',560066,1323567775,'def@gmail.com'),
+('Gagana','Gowri','9th cross','Hydrabad','Telangana',450065,5478908765,'ghi@gmail.com'),
+('Shree','karan','4th cross','Jaipur','Rajasthan',660065,5243458745,'jkl@gmail.com');
+
