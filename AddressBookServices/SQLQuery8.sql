7c46fd10046a8cb00e6ca6b1b45a807caedf8cd0
shreegowri <gaganandh@gmail.com>
2022-07-15 22:17:22 +0530

    [shreegowri]Ability to retrive all the entries sorted alpahbetically by person's name by city

---
+++ AddressBookServices/SQLQuery8.sql
@@ -0,0 +1,8 @@
+----------UC8 Retrive the entires sorted alphabetically by the persons name for given city--------
+SELECT FirstName,City from AddressBook ORDER BY FirstName;
+
+----to retrive all the data alphabetically order by firstname--------
+SELECT * from AddressBook ORDER BY FirstName;
+
+----to retrive all the data alphabetically order by City--------
+SELECT * from AddressBook ORDER BY City;
