a029bf4a50167cd17bfe41b250e453ed36afd40d
shreegowri <gaganandh@gmail.com>
2022-07-15 11:38:35 +0530

    [shreegowri]refactored code

---
+++ AddressBookServices/dbo.AddressBook.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[AddressBook] (
+    [FirstName]   VARCHAR (200)  NULL,
+    [LastName]    VARCHAR (200)  NULL,
+    [Address]     VARCHAR (1000) NULL,
+    [City]        CHAR (100)     NULL,
+    [State]       CHAR (100)     NULL,
+    [Zip]         BIGINT         NULL,
+    [PhoneNumber] BIGINT         NULL,
+    [Email]       VARCHAR (200)  NULL
+);
+
