9fb04c65d0b6e042a0f3d9b23c472969ac24f6d4
shreegowri <gaganandh@gmail.com>
2022-07-15 12:08:41 +0530

    [shreegowri]retriveing the person name based on the cty or state

---
+++ AddressBookServices/SQLQuery6.sql
@@ -0,0 +1,8 @@
+﻿select * from AddressBook;
+-----------inserted some more data to execute the uc--------------
+INSERT INTO AddressBook
+Values('shankar','rana','9th main','Bangaluru','Karnataka',560087,1234567897,'jkl@gmail.com'),
+('karan','Kumar','10th main','Bangaluru','Karnataka',560097,1234567807,'mno@gmail.com'),
+('hasini','renyu','11th main','Hydrabad','Telangana',561237,1653267897,'pqr@gmail.com');
+-------------UC6-retrive the person bolonging to coty or state--------------
+SELECT FirstName From AddressBook where  City='Bangaluru' or State='Karnataka';
