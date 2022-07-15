a3ffdbba53b5730df7517aed8ef269b52bdb4ac0
shreegowri <gaganandh@gmail.com>
2022-07-15 23:07:22 +0530

    [shreegowri]ability to add the person with the same name but different type

---
+++ AddressBookServices/SQLQuery11.sql
@@ -0,0 +1,6 @@
+----------uc11 - ability to add the person to both the family and friends----
+INSERT INTO AddressBook
+VALUES('Sita','Rama','6th cross','Wizag','Andrapradesh',765434,457545809,'vwx@gmail.com','Family'),
+('Sita','Rama','6th cross','Wizag','Andrapradesh',765434,457545809,'vwx@gmail.com','Friends')
+
+Select * from AddressBook;
